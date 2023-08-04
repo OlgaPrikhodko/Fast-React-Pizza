ea61d5a36dcf03af3fca1e73712f0b483bdd811d
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-04 12:02:14 +0300

    ⭐ feat(app): add Layout Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import AppLayout from "./ui/AppLayout";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
 import Order from "./features/order/Order";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
   },
 ]);
 
