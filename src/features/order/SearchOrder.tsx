11098e71c9a84899a7ad2df49cf8e896e08df0a7
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-05 09:07:04 +0300

    ⭐ feat(Header):  add search Order input

---
+++ src/features/order/SearchOrder.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SearchOrder: React.FC = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!query) return;
+
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchOrder;
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,9 +1,12 @@
+import SearchOrder from "@/features/order/SearchOrder";
 import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+
+      <SearchOrder />
 
       <p>Olha</p>
     </header>
