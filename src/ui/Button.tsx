92d283ee328449058782d25a9fe343f501cfc6b1
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-07 11:16:36 +0300

    💄styles: Header styling, add Button component

---
+++ src/ui/Button.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+interface ButtonProps {
+  to?: string;
+  disabled?: boolean;
+  children: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  to,
+  disabled = false,
+}) => {
+  const className =
+    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4";
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
+  return (
+    <button disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
+};
