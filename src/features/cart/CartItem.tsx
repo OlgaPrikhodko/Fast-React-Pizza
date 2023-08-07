9eb2121a9cc8a080657438aa2633134ed7d44865
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-07 14:02:31 +0300

    💄styles: Cart styling

---
+++ src/features/cart/CartItem.tsx
@@ -0,0 +1,26 @@
+import { Cart } from "@/types/orderTypes";
+import { Button } from "@/ui/Button";
+import { formatCurrency } from "@/utils/helpers";
+
+interface CartItemProps {
+  item: Cart;
+}
+
+export const CartItem: React.FC<CartItemProps> = ({ item }) => {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
+        {quantity}&times; {name}
+      </p>
+      <div className="flex items-center justify-between gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+
+        <Button type="small">Delete</Button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
