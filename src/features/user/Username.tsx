beca5e9e273cc9a56cb34a41afd3123550225389
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-08 10:04:53 +0300

    🏬 store: add user state

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,3 +1,12 @@
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+
 export const Username: React.FC = () => {
-  return <div className="hidden text-sm font-semibold md:block">Olha</div>;
+  const username = useSelector((state: RootState) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 };
---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,19 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action: PayloadAction<string>) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer;
