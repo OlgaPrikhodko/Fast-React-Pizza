e930aa75621f4853ac7ecd49e55ea27193c9d877
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-08-10 14:48:05 +0300

    ⭐ feat(CreateOrder):  integrate geolocation

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -33,7 +33,7 @@ export const fetchAddress = createAsyncThunk(
 interface InitialStateType {
   username: string;
   status: "idle" | "loading" | "error";
-  position: Coords | {};
+  position: Coords | undefined;
   address: string;
   error: string;
 }
@@ -41,7 +41,7 @@ interface InitialStateType {
 const initialState: InitialStateType = {
   username: "",
   status: "idle",
-  position: {},
+  position: undefined,
   address: "",
   error: "",
 };
@@ -64,9 +64,10 @@ const userSlice = createSlice({
         state.address = action.payload.address;
         state.status = "idle";
       })
-      .addCase(fetchAddress.rejected, (state, action) => {
+      .addCase(fetchAddress.rejected, (state) => {
         state.status = "error";
-        state.error = action.error.message ?? "";
+        state.error =
+          "There was a problem getting your address. Make sure to fill this field";
       }),
 });
 
